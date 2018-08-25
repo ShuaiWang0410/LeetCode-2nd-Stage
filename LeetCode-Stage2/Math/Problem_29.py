d6a740f94148fff44fd16c9147c2a0c66242bf96
ShuaiWang0410 <shuaiwang0410@gmail.com>
2018-08-25 00:25:12 -0400

    Problem_29_solved

--- LeetCode-Stage2/Math/Problem_29.py
+++ LeetCode-Stage2/Math/Problem_29.py
@@ -15,29 +15,75 @@ class Solution(object):
         :rtype: int
         """
 
+        max = 2147483647
+        nmax = -2147483648
         if 0 == dividend:
             return 0
 
         if 1 == divisor:
+            if dividend > max or dividend < nmax:
+                return max
             return dividend
+
+        if -1 == divisor:
+            if dividend > max + 1 or dividend <= nmax:
+                return max
+            return -dividend
+
 
         neg_flag = False
         if 0 > dividend * divisor:
-            if -1 == divisor:
-                return -dividend
 
             neg_flag = True
+            if dividend < 0:
+                dividend = -dividend
+            if divisor < 0:
+                divisor = -divisor
+        elif dividend < 0 and divisor < 0:
             dividend = -dividend
+            divisor = -divisor
 
-
-        times = 0
+        t_divisor = divisor
+        result = 0
+        t = 0
         while True:
-            if 0 == dividend or abs(dividend) < abs(divisor):
-                if neg_flag:
-                    return -times
-                return times
-            dividend -= divisor
-            times += 1
+
+            if dividend < t_divisor and t == 0:
+                break
+
+            if dividend > t_divisor:
+                t_divisor <<= 1
+                t += 1
+            else:
+                if dividend < t_divisor:
+                    t -= 1
+                    t_divisor >>= 1
+                    result += pow(2,t)
+                    dividend -= t_divisor
+                    t_divisor = divisor
+                    t = 0
+                else:
+                    result += pow(2,t)
+                    break
+
+
+        if neg_flag:
+            if -result < nmax:
+                return max
+            return -result
+        if result > max:
+            return max
+        return result
+
+
+
+
+
+
+
+
+
+
 
 c = Solution()
-print(c.divide(0,-3))
+print(c.divide(-2147483648,-1))
